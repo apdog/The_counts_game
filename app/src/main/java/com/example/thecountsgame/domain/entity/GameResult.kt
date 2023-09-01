151cd5a1b9c68c3865f3de2ce8ae841dc2286515
apdog <alex.puhow@gmail.com>
2023-09-01 18:57:43 +0300

    Замена Serialazable на Parcelable + GameViewModel

--- app/src/main/java/com/example/thecountsgame/domain/entity/GameResult.kt
+++ app/src/main/java/com/example/thecountsgame/domain/entity/GameResult.kt
@@ -1,10 +1,12 @@
 package com.example.thecountsgame.domain.entity
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class GameResult (
     val winner: Boolean,
     val countOfRightsAnswers: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-) : Serializable
+) : Parcelable
