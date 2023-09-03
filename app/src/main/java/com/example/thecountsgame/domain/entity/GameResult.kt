823f431ce23ec7797bae353d37c6d7d14e104b25
apdog <alex.puhow@gmail.com>
2023-09-03 18:49:56 +0300

    переход на DataBinding

--- app/src/main/java/com/example/thecountsgame/domain/entity/GameResult.kt
+++ app/src/main/java/com/example/thecountsgame/domain/entity/GameResult.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class GameResult (
     val winner: Boolean,
-    val countOfRightsAnswers: Int,
+    val countOfRightAnswers: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
 ) : Parcelable
--- app/src/main/java/com/example/thecountsgame/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/thecountsgame/presentation/GameFinishedFragment.kt
@@ -5,15 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.thecountsgame.R
-import com.example.thecountsgame.databinding.FragmentChooseLevelBinding
 import com.example.thecountsgame.databinding.FragmentGameFinishedBinding
-import com.example.thecountsgame.databinding.FragmentWelcomeBinding
-import com.example.thecountsgame.domain.entity.GameResult
 import java.lang.RuntimeException
 
 class GameFinishedFragment : Fragment() {
@@ -35,50 +30,12 @@ class GameFinishedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupClickListeners()
-        bindViews()
+        binding.gameResult = args.gameResult
     }
 
     private fun setupClickListeners() {
         binding.buttonRetry.setOnClickListener {
             retryGame()
-        }
-    }
-
-    private fun bindViews() {
-        with(binding) {
-            emojiResult.setImageResource(getSmileResId())
-            tvRequiredAnswers.text = String.format(
-                getString(R.string.required_score),
-                args.gameResult.gameSettings.minCountOfRightAnswers
-            )
-            tvScoreAnswers.text = String.format(
-                getString(R.string.score_answers),
-                args.gameResult.countOfRightsAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.required_percentage),
-                args.gameResult.gameSettings.minPercentOfRightAnswers
-            )
-            tvScorePercentage.text = String.format(
-                getString(R.string.score_percentage),
-                getPercentOfRightAnswers()
-            )
-        }
-    }
-
-    private fun getSmileResId(): Int {
-        return if (args.gameResult.winner) {
-            R.drawable.ic_smile
-        } else {
-            R.drawable.ic_sad
-        }
-    }
-
-    private fun getPercentOfRightAnswers() = with(args.gameResult) {
-        if (countOfQuestions == 0) {
-            0
-        } else {
-            ((countOfRightsAnswers / countOfQuestions.toDouble()) * 100).toInt()
         }
     }
 
